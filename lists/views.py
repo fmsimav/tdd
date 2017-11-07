5e3e79240599d71b71326c5364602b209f19e98f
fms <murat@muratsimav.com>
2017-11-08 00:00:12 +0300

    listeleri göstermek için yeni url, view ve template oluşturuldu

--- lists/views.py
+++ lists/views.py
@@ -4,6 +4,9 @@ from lists.models import Item
 def home_page(request):
     if request.method == 'POST':
         Item.objects.create(text=request.POST.get('item_text', ''))
-        return redirect('/')
+        return redirect('/lists/the-only-list-in-the-world/')
+    return render(request, 'lists/home.html')
+
+def view_list(request):
     items = Item.objects.all()
-    return render(request, 'lists/home.html', context={'items': items})
+    return render(request, 'lists/list.html', context={'items': items})
